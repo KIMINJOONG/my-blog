6703a85601070bc871f6f609babd9bce013c5789
KIMINJOONG <ijboym216@gmail.com>
2019-03-02 17:02:07 +0900

    setting client

--- client/src/backup.js
+++ client/src/backup.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   state = {
@@ -33,21 +31,7 @@ class App extends Component {
   };
 render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
         <p>{this.state.response}</p>
         <form onSubmit={this.handleSubmit}>
           <p>
@@ -66,4 +50,3 @@ render() {
   }
 }
 
-export default App;
