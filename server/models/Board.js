c0e1e790f861af691ace85ebddfce5680cba7387
KIMINJOONG <ijboym216@gmail.com>
2019-04-25 07:19:08 +0900

    add columns in Board.js

--- server/models/Board.js
+++ server/models/Board.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 
 const BoardSchema = new mongoose.Schema({
-    title: {
-        type: String
-    },
-    description: String
+  title: {
+    type: String
+  },
+  description: String,
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
 });
 
 const model = mongoose.model("Board", BoardSchema);
