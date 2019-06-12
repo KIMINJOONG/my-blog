4296369d18adc3e5bb6bb7a41fece9c829062855
KIMINJOONG <ijboym216@gmail.com>
2019-06-13 00:24:09 +0900

    i try cookie parser

--- server/app.js
+++ server/app.js
@@ -7,11 +7,12 @@ import routes from "./routes";
 import cors from "cors";
 import bodyParser from "body-parser";
 import decodeJWT from "./utils/decodeJWT";
+import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const app = express();
 const jwt = async (req, res, next) => {
   const token = req.get("X-JWT");
-  console.log(token);
   if (toekn) {
     const user = await decodeJWT(token);
     if (user) {
@@ -26,6 +27,7 @@ const jwt = async (req, res, next) => {
 };
 app.use(helmet());
 app.use(cors());
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
